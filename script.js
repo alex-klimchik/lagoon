0dfa16300d0c04349d06eca00a6ed6d7639e9d2e
Alex <alex.lytvyn.13@gmail.com>
2020-03-12 11:15:18 +0200

    Fixed adaptive modal window

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
 
     // открытие модального окна для Входа
     $('.buttons-enter, .mobile-enter').click(() => {
+        $('body').addClass('overflow-hidden');
         $('#enter').css('display', 'block');
         $('#recovery').css('display', 'none');
         $('#toRegister').css('display', 'none');
@@ -23,7 +24,7 @@ $(document).ready(function () {
             if (!div.is(e.target)
                 && div.has(e.target).length === 0) {
                 $('.overlay').fadeOut();
-
+                $('body').removeClass('overflow-hidden');
                 //удаление события закрытия модального окна
                 $(this).off('mousedown');
             }
@@ -63,39 +64,64 @@ $(document).ready(function () {
     $('.mobile-navbar').click(() => {
         $('header nav .navbar>ul').toggleClass('d-none');
     });
+    // Кнопка "Закрыть меню"
+    $('.menu_close').click(() => {
+        $('header nav .navbar>ul').toggleClass('d-none');
+    })
 
+    // Выбор Скайпа или телеграма в форме регистрации
+    $('.select-type .dropdown-menu a').click(function (evt) {
+        evt.preventDefault();
+        let selectedElement = $(this).attr('value');
+        let inp = $(this).closest('.select-type').find('input[name="skypeOrTg"]');
+        inp.attr('placeholder', selectedElement);
+    });
+
+    // Кнопка "Регистрация" на главном экране 
     $('.btn-bk-forms').click(function (e) {
         e.preventDefault();
         let form = $(this).closest('form');
-        let email, login, name, skype, telegram, password, confirm_password, type;
+        let email, login, name, skype, telegram, skypeOrTgInput, selectedEl, password, confirm_password, type;
         email = $(form).find('input[name="e-mail"]').val();
         login = $(form).find('input[name="login"]').val();
         name = $(form).find('input[name="name"]').val();
-        skype = $(form).find('input[name="skype"]').val();
         password = $(form).find('input[name="password"]').val();
         confirm_password = $(form).find('input[name="confirm_password"]').val();
         type = $(form).find('.choice-person input[type="radio"]:checked').val();
-        $.ajax({
-            "url": "http://lagoon.me/api/register_user",
-            "method": "post",
-            "crossDomain": true,
-            "dataType": 'json',
-            "data": {
-                "email": email,
-                "login": login,
-                "name": name,
-                "skype": skype,
-                "password": password,
-                "confirm_password": confirm_password,
-                "type": type,
-            },
-            "success": function (data) {
-                console.log('success', data)
-            },
-            "error": function (data) {
-                console.log('error', data)
-            },
-        });
-        console.log(email, login, name, skype, telegram, password, confirm_password, type);
+
+        skypeOrTgInput = $(form).find('input[name="skypeOrTg"]');
+        selectedEl = skypeOrTgInput.attr('placeholder');
+        if (selectedEl == "Skype") {
+            skype = $(skypeOrTgInput).val() || '';
+            telegram = '';
+        } else if (selectedEl == "Telegram") {
+            telegram = $(skypeOrTgInput).val() || '';
+            skype = '';
+        }
+        console.log('skype=' + skype, 'telegram=' + telegram);
+        // $.ajax({
+        //     "url": "http://lagoon.me/api/register_user",
+        //     "method": "post",
+        //     "crossDomain": true,
+        //     "dataType": 'json',
+        //     "data": {
+        //         "email": email,
+        //         "login": login,
+        //         "name": name,
+        //         "skype": skype,
+        //         "telegram": telegram,
+        //         "password": password,
+        //         "confirm_password": confirm_password,
+        //         "type": type,
+        //     },
+        //     "success": function (data) {
+        //         console.log('success', data)
+        //     },
+        //     "error": function (data) {
+        //         console.log('error', data)
+        //     },
+        // });
+        // console.log(email, login, name, skype, telegram, password, confirm_password, type);
     });
+
 });
